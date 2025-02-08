ec915e176cc52a4201786a9eafed0d47a9412be9
Kelvin Mock <kelvin.mock@gmail.com>
2025-02-08 18:49:45 -0500

    Function Definition

--- evalDeck.py
+++ evalDeck.py
@@ -1,7 +1,7 @@
 from pokerHierarchy import PokerHierarchy
 import tg.types as types
 
-def evalDeck(state: types.PokerSharedState, hand):
+def evalDeck(state: types.PokerSharedState, hand: types.Card):
     """
     Evaluate the combination of Shared State and the player's hand.
     Return an Integer from the PokerHierarchy of the best combination.
