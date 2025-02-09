2496788b225ea630e7dbdf2d9bb6f360acf1e4c0
Kelvin Mock <kelvin.mock@gmail.com>
2025-02-09 11:13:20 -0500

    bet strength algorithm

--- betStrength.py
+++ betStrength.py
@@ -1,4 +1,7 @@
-def betStrenth(state):
+from tg.types import PokerSharedState
+from evalDeck import evalDeck
+
+def betStrenth(state: PokerSharedState) -> int:
     """
     return a number that tells us how strong we think our opponent's hand
     is depending on their bet sizing. we should be comparing their bet sizing
@@ -10,4 +13,15 @@ def betStrenth(state):
     :return: int
     
     """
-    # TODO implement
+    pot_size: int = state.pot
+    opponent_bet: int = state.target_bet
+    relative_bet_size: int = opponent_bet / pot_size
+
+    if relative_bet_size > 0.8:
+        return 4    # Opponent likely has a strong hand.
+    elif relative_bet_size > 0.4:
+        return 3    # Consider folding weaker hands.
+    elif relative_bet_size > 0.1:
+        return 2    # Proceed cautiously.
+    else:
+        return 1    # Consider bluffing or raising.
