c8b0a0156c0e60f6a5f18073490316db5fe105d6
Kelvin Mock <kelvin.mock@gmail.com>
2025-02-08 20:12:57 -0500

    Added test case for flush checking

--- tests/testEvalDeck.py
+++ tests/testEvalDeck.py
@@ -27,7 +27,7 @@ class TestEvalDeck(unittest.TestCase):
         ]
         self.assertTrue(isFlush(state, hand))
 
-    def test_is_flush_false(self):
+    def test_is_flush_false_using_all_cards(self):
         state = types.PokerSharedState(
             dealer_position=0, 
             small_blind=0, 
@@ -41,6 +41,30 @@ class TestEvalDeck(unittest.TestCase):
                 types.Card(types.Rank.THREE, types.Suit.HEARTS), 
                 types.Card(types.Rank.FOUR, types.Suit.HEARTS), 
                 types.Card(types.Rank.FIVE, types.Suit.HEARTS)
+            ]
+        )
+        hand = [
+            types.Card(types.Rank.ACE, types.Suit.HEARTS), 
+            types.Card(types.Rank.TWO, types.Suit.CLUBS)
+        ]
+        self.assertFalse(isFlush(state, hand))
+
+    def test_is_flush_false_have_spare_cards(self):
+        state = types.PokerSharedState(
+            dealer_position=0, 
+            small_blind=0, 
+            big_blind=0, 
+            pot=0, 
+            target_bet=0, 
+            players=[], 
+            round=types.PokerRound.RIVER, 
+            done=False, 
+            cards=[
+                types.Card(types.Rank.THREE, types.Suit.HEARTS), 
+                types.Card(types.Rank.FOUR, types.Suit.HEARTS), 
+                types.Card(types.Rank.FIVE, types.Suit.HEARTS),
+                types.Card(types.Rank.JACK, types.Suit.SPADES),
+                types.Card(types.Rank.KING, types.Suit.CLUBS)
             ]
         )
         hand = [
